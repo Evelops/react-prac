55b6f8b2998f352d52a3e33b698b3578953ae4ac
eomseung-yeol <co6747@naver.com>
2023-05-01 13:00:54 +0900

    Feat: 추가 tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css'
 
 function App() {
   return (
     <div className="App">
+      <div className='flex bg-black pl-4'>
+        <h4 className='text-white'>블로그임</h4>
+      </div>
     </div>
   );
 }
